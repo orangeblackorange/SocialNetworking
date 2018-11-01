182af72fe4b5ba467788cde2373c6ef7b32e9812
orangeblack <orangeblack>
2018-11-01 14:01:51 +0000

    Add the wall command

--- SocialNetworkingLibrary/SocialNetworkingService.cs
+++ SocialNetworkingLibrary/SocialNetworkingService.cs
@@ -111,6 +111,51 @@ namespace SocialNetworkingLibrary
         }
     }
 
+    public class CommandWall : ICommand
+    {
+        List<Post> posts = null;
+        IConsoleWriter writer = null;
+        private Dictionary<string, List<string>> followers = null;
+        public CommandWall(List<Post> posts, Dictionary<string, List<string>> followers, IConsoleWriter writer)
+        {
+            this.posts = posts;
+            this.writer = writer;
+            this.followers = followers;
+        }
+
+        public void Process(string input)
+        {
+
+            var matchWallResult = Regex.Match(input, @"(?<username>\w+) wall");
+            if (matchWallResult.Success)
+            {
+                var username = matchWallResult.Groups["username"].Value;
+                WriteAllFoundPosts(username);
+
+                if (followers.ContainsKey(username))
+                {
+                    var foundFollowers = followers[username];
+
+                    foreach (var follower in foundFollowers)
+                    {
+                        WriteAllFoundPosts(follower);
+                    }
+                }
+
+            }
+        }
+
+        private void WriteAllFoundPosts(string username)
+        {
+            var found = posts.FindAll(p => p.UserName.Equals(username));
+
+            foreach (var post in found)
+            {
+                writer.WriteLine(post);
+            }
+        }
+    }
+
     public class Post
     {
         public String UserName { get; set; }
---
+++ Testing/CommandWallTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SocialNetworkingLibrary;
+
+namespace Testing
+{
+    [TestFixture]
+    class CommandWallTest
+    {
+        [Test]
+        public void CommandWallNoFollowersTest()
+        {
+            var resultPosts = new List<Post>();
+            resultPosts.Add(new Post { UserName = "Bob", Message = "Good game though." });
+            resultPosts.Add(new Post { UserName = "Charlie", Message = "I'm in New York today! Anyone wants to have a coffee?" });
+            resultPosts.Add(new Post { UserName = "Bob", Message = "Damn!We lost!" });
+
+            var following = new Dictionary<string, List<string>>();
+
+            string consoleInput = "Bob wall";
+
+            var writerMock = new Mock<IConsoleWriter>();
+
+            var command = new CommandWall(resultPosts, following, writerMock.Object);
+            command.Process(consoleInput);
+
+            writerMock.Verify(m => m.WriteLine(resultPosts[0]));
+            writerMock.Verify(m => m.WriteLine(resultPosts[2]));
+        }
+
+        [Test]
+        public void CommandWallWithFollowerTest()
+        {
+            var resultPosts = new List<Post>();
+            resultPosts.Add(new Post { UserName = "Bob", Message = "Good game though." });
+            resultPosts.Add(new Post { UserName = "Charlie", Message = "I'm in New York today! Anyone wants to have a coffee?" });
+            resultPosts.Add(new Post { UserName = "Bob", Message = "Damn!We lost!" });
+
+            var following = new Dictionary<string, List<string>>();
+            var list = new List<string>();
+            list.Add("Charlie");
+
+            following.Add("Bob", list);
+
+            string consoleInput = "Bob wall";
+
+            var writerMock = new Mock<IConsoleWriter>();
+
+            var command = new CommandWall(resultPosts, following, writerMock.Object);
+            command.Process(consoleInput);
+
+            writerMock.Verify(m => m.WriteLine(resultPosts[0]));
+            writerMock.Verify(m => m.WriteLine(resultPosts[2]));
+            writerMock.Verify(m => m.WriteLine(resultPosts[1]));
+        }
+    }
+}

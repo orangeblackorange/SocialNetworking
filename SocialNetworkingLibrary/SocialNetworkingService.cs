9b712dceb30f222a49f968e307b51493f23b456b
orangeblack <orangeblack>
2018-11-01 08:39:32 +0000

    Add a posting command

---
+++ SocialNetworkingLibrary/SocialNetworkingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SocialNetworkingLibrary
+{
+    public class SocialNetworkingService
+    {
+        public Post Process(string input)
+        {
+            Post result = null;
+
+            var matchResult = Regex.Match(input, @"(?<username>\w+) -> (?<message>([\w\s]*))");
+            if (matchResult.Success)
+            {
+                var username = matchResult.Groups["username"].Value;
+                var message = matchResult.Groups["message"].Value;
+                result = new Post { UserName = username, Message = message };
+            }
+
+            return result;
+        }
+    }
+
+    public class Post
+    {
+        public String UserName { get; set; }
+        public String Message { get; set; }
+    }
+
+}
---
+++ Testing/SocialNetworkingServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SocialNetworkingLibrary;
+
+namespace Testing
+{
+    [TestFixture]
+    public class SocialNetworkingServiceTest
+    {
+        [Test]
+        public void AddPostTest()
+        {
+            string consoleInput = "Alice -> I love the weather today";
+
+            var service = new SocialNetworkingService();
+            var post = service.Process(consoleInput);
+
+            Assert.AreEqual("Alice", post.UserName);
+            Assert.AreEqual("I love the weather today", post.Message);
+        }
+    }
+}
+
+//capture input
+//Process input
+//    Posting: Add to the list(post{ whom, message, time})
+//	  reading: fetch posts from list by whom
+//    following: for this username add a following. Extend object (post{whom, message, time, following})
+//	  wall: fetch posts from list by whom and then "following" list
